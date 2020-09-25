ebb8fc2c019508ba9776cf38b8292a3718484247
The Solo Artist <michael.solorzano@email.bakersfieldcollege.edu>
2020-09-24 22:58:57 -0700

    updated styling and themes/colors

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,28 +1,28 @@
   
 var friends = [
 	{
-		name: "Charlie",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		name: "Gohan",
+		photo: "https://w7.pngwing.com/pngs/824/743/png-transparent-gohan-piccolo-goku-cell-vegeta-goku-gohan-piccolo-goku.png",
 		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
 	},
 	{
-		name: "Sweet Dee",
-		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
+		name: "Goku",
+		photo: "https://vignette.wikia.nocookie.net/character-level/images/f/f1/Goku_universe_survival_by_koku78-dbg6nkx.png/revision/latest/scale-to-width-down/340?cb=20190817185125",
 		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
 	},
 	{
-		name: "Dennis",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
+		name: "Yamcha",
+		photo: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/84dc13b7-a2e7-4b45-83ec-311e72e82900/ddmjua0-49ae0baf-d86f-48c3-bf34-0f293b5f8108.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvODRkYzEzYjctYTJlNy00YjQ1LTgzZWMtMzExZTcyZTgyOTAwXC9kZG1qdWEwLTQ5YWUwYmFmLWQ4NmYtNDhjMy1iZjM0LTBmMjkzYjVmODEwOC5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.JIRm2a_dELphiheU3KaUlLieal_aLNrz8S-_H5HF5j0",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
 	},
 	{
-		name: "Mac",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
+		name: "Krillin",
+		photo: "https://static.wikia.nocookie.net/bnhaff/images/1/18/Ddmju9h-915eafc1-bca9-4441-8d33-6f031d5d983a.png/revision/latest?cb=20200216223327",
 		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
 	},
 	{
-		name: "Frank",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		name: "Master Roshi",
+		photo: "https://vignette.wikia.nocookie.net/characterprofile/images/c/c2/Roshi.jpg/revision/latest?cb=20190410143026",
 		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
 	}
 ];
